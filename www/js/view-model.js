c455c1408933d7371cd54155b41a7d04a687a4f9
Yamaroni <yamaroni8@gmail.com>
2022-06-07 09:13:42 +0700

    added orientation features

--- www/js/view-model.js
+++ www/js/view-model.js
@@ -5,4 +5,7 @@ const viewModel = [{
 }, {
     path: '/features/statusbar/',
     componentUrl: `./pages/features/statusbar.html?v=${Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15)}`,
+}, {
+    path: '/features/orientation/',
+    componentUrl: `./pages/features/orientation.html?v=${Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15)}`,
 }]
