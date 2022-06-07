5fc4ca95f4ef64f203d9b72ba6120d1ec4bb6da9
Yamaroni <yamaroni8@gmail.com>
2022-06-07 08:46:25 +0700

    initiate framework7 configuration

---
+++ www/js/view-model.js
@@ -0,0 +1,5 @@
+"user strict"
+const viewModel = [{
+    path: '/',
+    componentUrl: `./pages/main.html?v=${Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15)}`,
+}]
