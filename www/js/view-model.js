fde1288e803c74dd8c25f683d5948503e85baf26
Yamaroni <yamaroni8@gmail.com>
2022-06-07 09:12:27 +0700

    added statusbar features

--- www/js/view-model.js
+++ www/js/view-model.js
@@ -2,4 +2,7 @@
 const viewModel = [{
     path: '/',
     componentUrl: `./pages/main.html?v=${Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15)}`,
+}, {
+    path: '/features/statusbar/',
+    componentUrl: `./pages/features/statusbar.html?v=${Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15)}`,
 }]
